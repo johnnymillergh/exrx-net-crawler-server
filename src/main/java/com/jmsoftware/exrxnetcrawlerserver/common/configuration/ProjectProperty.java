e0429e1a892ae8abc077dda4d9d55c82286688c5
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-01-21 23:50:59 +0800

    refactor($ProjectProperty): refine ProjectProperty.java

--- src/main/java/com/jmsoftware/exrxnetcrawlerserver/common/configuration/ProjectProperty.java
+++ src/main/java/com/jmsoftware/exrxnetcrawlerserver/common/configuration/ProjectProperty.java
@@ -17,6 +17,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Description: ProjectProperty, change description here.
@@ -54,12 +55,14 @@ public class ProjectProperty {
      * Init properties after dependencies injection is done.
      */
     @PostConstruct
-    public void init() {
+    public void postConstruct() {
         log.info("Start to init properties after dependency injection was done.");
         this.model = this.parsePom();
-        assert this.model != null;
-        this.licenses = this.model.getLicenses();
-        this.developers = this.model.getDevelopers();
+        var optionalModel = Optional.ofNullable(this.model);
+        optionalModel.ifPresent(model1 -> {
+            this.licenses = model1.getLicenses();
+            this.developers = model1.getDevelopers();
+        });
     }
 
     /**
@@ -71,9 +74,11 @@ public class ProjectProperty {
     public void onApplicationReady(ApplicationReadyEvent event) {
         log.info("Start to init properties. On received event: {}", event.toString());
         this.model = this.parsePom();
-        assert this.model != null;
-        this.licenses = this.model.getLicenses();
-        this.developers = this.model.getDevelopers();
+        var optionalModel = Optional.ofNullable(this.model);
+        optionalModel.ifPresent(model1 -> {
+            this.licenses = model1.getLicenses();
+            this.developers = model1.getDevelopers();
+        });
     }
 
     /**
@@ -82,23 +87,24 @@ public class ProjectProperty {
      * @return Project model
      */
     private Model parsePom() {
-        MavenXpp3Reader reader = new MavenXpp3Reader();
+        var reader = new MavenXpp3Reader();
         // Read pom.xml from relative path
         try {
-            File pom = new File(POM_FILE_NAME);
+            var pom = new File(POM_FILE_NAME);
             if (pom.exists()) {
+                log.info("Found 'pom.xml'. POM path = {}", pom.getPath());
                 return reader.read(new FileReader(pom));
             }
         } catch (IOException | XmlPullParserException e) {
             log.error("Error occurred when read pom.xml. Filepath: {}", POM_FILE_NAME, e);
         }
         // Read pom.xml from packaged jar
-        String pomFilePath = "/META-INF/maven/" + groupId + "/" + artifactId + "/" + POM_FILE_NAME;
-        log.error("Read pom.xml from packaged jar. Filepath = {}", pomFilePath);
+        var pomFilePath = String.format("/META-INF/maven/%s/%s/%s", groupId, artifactId, POM_FILE_NAME);
+        log.info("Read 'pom.xml' from packaged jar. Filepath = {}", pomFilePath);
         try {
             return reader.read(
                     new InputStreamReader(ProjectProperty.class.getResourceAsStream(pomFilePath))
-            );
+                              );
         } catch (IOException | XmlPullParserException e) {
             log.error("Error occurred when read pom.xml. Filepath: {}", pomFilePath, e);
         }
