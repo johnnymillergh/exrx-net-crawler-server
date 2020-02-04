24e8b364ca38f26feba691159af96c98afafcd90
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-02-04 12:24:17 +0800

    feat($Common): provide API for retrieving application info

--- src/main/java/com/jmsoftware/exrxnetcrawlerserver/common/controller/CommonController.java
+++ src/main/java/com/jmsoftware/exrxnetcrawlerserver/common/controller/CommonController.java
@@ -1,6 +1,14 @@
 package com.jmsoftware.exrxnetcrawlerserver.common.controller;
 
+import com.jmsoftware.exrxnetcrawlerserver.common.bean.ResponseBodyBean;
+import com.jmsoftware.exrxnetcrawlerserver.common.service.CommonService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 /**
  * <h1>CommonController</h1>
@@ -11,5 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2/4/20 10:29 AM
  **/
 @RestController
+@RequiredArgsConstructor
+@Api(tags = {"Common Controller"})
 public class CommonController {
+    private final CommonService commonService;
+
+    @GetMapping("/app-info")
+    @ApiOperation(value = "/app-info", notes = "Retrieve application information")
+    public ResponseBodyBean<Map<String, Object>> applicationInformation() {
+        var data = commonService.getApplicationInfo();
+        return ResponseBodyBean.ofDataAndMessage(data, "Succeed to retrieve app info.");
+    }
 }
